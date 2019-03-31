31d5a3a9ffae9546b7fedd8ab1c1f05f7f5abace
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-31 19:38:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,7 +13,7 @@ array.sort{|x, y| x.length <=> y.length} #sorting x <=> y based on length.
 end
 
 def swap_elements(array)
-array[1], array[2] = array[2], array[1]
+arrray[0], array[1], array[2] = array[0] array[2], array[1]
 end
 
 def reverse_array(array)
