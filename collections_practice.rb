b26caf801f7c50e304cd126fdf16d9b71bda4d06
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-31 19:36:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,10 +9,13 @@ end
 end
 
 def sort_array_char_count(array)
-array.sort{|x, y| x.length <=> y.length}
+array.sort{|x, y| x.length <=> y.length} #sorting x <=> y based on length. 
 end
 
+def swap_elements(array)
+array[1], array[2] = array[2], array[1]
+end
 
 def reverse_array(array)
-  array.reverse
+  array.reverse 
 end
